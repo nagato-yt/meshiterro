1eaa761157bec252355177ce5690715413461a41
nagato-yt <119833402+nagato-yt@users.noreply.github.com>
2023-01-14 04:30:47 +0000

    [Finish]4-15

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,10 +9,10 @@ class User < ApplicationRecord
           
           has_one_attached :profile_image
           
-          def get_profile_image
+          def get_profile_image(width,height)
             unless profile_image.attached?
-              file_path =Rails.root.join('app/assets/sample-author1.jpg')
-              profile_image.attache(io:File.open(file_path),filename:'default-image.jpg',content_type:'image/jpeg')
+              file_path = Rails.root.join('app/assets/sample-author1.jpg')
+              profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
             end 
             profile_image.variant(resize_to_limit:[width,height]).processed
           end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :users,only:[:show,:edit]
-  resources :post_images,only:[:new,:index,:show,:create,:destroy]
- 
+ get 'homes/about' => 'homes#about',as:'about'
   devise_for :users
   root to: 'homes#top'
-  get 'homes/about' => 'homes#about',as:'about'
   
+   resources :post_images,only:[:new,:index,:show,:create,:destroy]
+   resources :users,only:[:show,:edit]
+ 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
