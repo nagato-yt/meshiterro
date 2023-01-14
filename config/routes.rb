3e400666ce4ab9eda1243a682fc070eb1ee8be39
nagato-yt <119833402+nagato-yt@users.noreply.github.com>
2023-01-14 11:11:17 +0000

    [Finish]4-18

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
- get 'homes/about' => 'homes#about',as:'about'
+ 
   devise_for :users
   root to: 'homes#top'
-  
-   resources :post_images,only:[:new,:index,:show,:create,:destroy]
-   resources :users,only:[:show,:edit,:update]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+   resources :post_comments, only: [:create,:destroy]
+  end 
+  resources :users, only: [:show, :edit, :update]
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
