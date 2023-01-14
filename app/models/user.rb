c9692b5d480dfca626f7ce1b03501c2a246d1ab2
nagato-yt <119833402+nagato-yt@users.noreply.github.com>
2023-01-14 06:44:27 +0000

    [Finish]4-16

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,11 @@ class User < ApplicationRecord
           
           has_one_attached :profile_image
           
-          def get_profile_image(width,height)
-            unless profile_image.attached?
-              file_path = Rails.root.join('app/assets/sample-author1.jpg')
-              profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
-            end 
-            profile_image.variant(resize_to_limit:[width,height]).processed
-          end 
+         def get_profile_image(width, height)
+           unless profile_image.attached?
+            file_path = Rails.root.join('app/assets/images/sample-author1.jpg')
+            profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+         end
+          profile_image.variant(resize_to_limit: [width, height]).processed
+         end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   root to: 'homes#top'
   
    resources :post_images,only:[:new,:index,:show,:create,:destroy]
-   resources :users,only:[:show,:edit]
+   resources :users,only:[:show,:edit,:update]
  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
