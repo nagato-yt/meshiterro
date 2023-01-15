b480dfccd0e3ee3c02fbe833c87604795dcd9b15
nagato-yt <119833402+nagato-yt@users.noreply.github.com>
2023-01-15 12:02:13 +0000

    [Finish]4-19

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -3,6 +3,9 @@ class PostImage < ApplicationRecord
     has_one_attached :image
     belongs_to :user
     has_many :post_comments, dependent: :destroy
+    has_many :favorites, dependent: :destroy
+        
+    
     
     
     def get_image
@@ -13,4 +16,9 @@ class PostImage < ApplicationRecord
         end
         image
     end 
+    
+    def favorited_by?(user)
+        favorites.exists?(user_id: user.id)
+    end 
+
 end
