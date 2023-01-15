c23b1902b507b9fa8a0bd7e41dff1add3f6ec093
nagato-yt <119833402+nagato-yt@users.noreply.github.com>
2023-01-15 12:49:22 +0000

    [Finish]4-21

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -4,7 +4,9 @@ class PostImage < ApplicationRecord
     belongs_to :user
     has_many :post_comments, dependent: :destroy
     has_many :favorites, dependent: :destroy
-        
+    
+    validates :shop_name, presence: true
+    validates :image,     presence: true
     
     
     
