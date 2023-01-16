6b73a8f94241c6a7979e23edf3c2cb8699b846f1
nagato-yt <119833402+nagato-yt@users.noreply.github.com>
2023-01-16 03:16:50 +0000

    [Finish]4-25

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,18 @@
 class UsersController < ApplicationController
   def show
+    is_matching_login_user
     @user= User.find(params[:id])
     @post_images= @user.post_images.page(params[:page])
   end
 
   def edit
+    is_matching_login_user 
     @user = User.find(params[:id])
   end
   
   def update
+    is_matching_login_user
+    
     @user =User.find(params[:id])
     @user.update(user_params)
     redirect_to user_path
@@ -19,6 +23,14 @@ class UsersController < ApplicationController
 
  def user_params
   params.require(:user).permit(:name,:profile_image)
- end
+ end 
+ 
+ def is_matching_login_user
+   user_id= params[:id].to_i
+   unless user_id ==current_user.id
+     redirect_to post_images_path
+   end 
+ end 
+
 
 end 
